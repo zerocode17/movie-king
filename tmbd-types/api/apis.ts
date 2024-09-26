6e7f77f87a39e1a3f7d2d02f5350ea0758bf1792
zerocode17 <ciro2510@gmail.com>
2024-09-26 00:01:34 -0300

    fixed typescript error

--- tmbd-types/api/apis.ts
+++ tmbd-types/api/apis.ts
@@ -1,14 +1,18 @@
-export * from './defaultApi';
-import { DefaultApi } from './defaultApi';
-import * as http from 'http';
+export * from "./defaultApi";
+import { DefaultApi } from "./defaultApi";
+import * as http from "http";
 
 export class HttpError extends Error {
-    constructor (public response: http.IncomingMessage, public body: any, public statusCode?: number) {
-        super('HTTP request failed');
-        this.name = 'HttpError';
-    }
+  constructor(
+    public response: http.IncomingMessage,
+    public body: any,
+    public statusCode?: number,
+  ) {
+    super("HTTP request failed");
+    this.name = "HttpError";
+  }
 }
 
-export { RequestFile } from '../model/models';
+export type { RequestFile } from "../model/models";
 
 export const APIS = [DefaultApi];
